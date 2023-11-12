ebce7b2a75829db46a8445d4554ff99ccec13647
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-11-11 16:04:45 -0800

    Add basic support for arrays

--- src/types.rs
+++ src/types.rs
@@ -1,14 +1,16 @@
 use std::fmt;
 use std::str::FromStr;
 
+use async_recursion::async_recursion;
 use heck::AsUpperCamelCase;
-use tokio_postgres::types::Type as PgType;
+use tokio_postgres::types::{ToSql, Type as PgType};
 use tokio_postgres::Client;
 
 use crate::Table;
 
 #[derive(Debug, PartialEq)]
 pub enum Type {
+    Array(Box<Type>),
     Builtin(PgType),
     Composite(Table),
     Enum(PgEnum),
@@ -19,12 +21,36 @@ impl Type {
         name: &str,
         client: &Client,
     ) -> Result<Self, tokio_postgres::Error> {
-        let sql = "SELECT oid, typtype FROM pg_catalog.pg_type WHERE typname = $1";
-        let row = client.query_one(sql, &[&name]).await?;
-        Ok(match row.get::<_, i8>(1) {
-            // enum: 'e' is 101 in ASCII, but b'e' can only be u8 in Rust
-            101 => Self::Enum(PgEnum::from_postgres(row.get(0), name, client).await?),
-            ty => todo!("unknown Postgres type type {ty:?}"),
+        Self::from_postgres("typname = $1", &[&name], client).await
+    }
+
+    #[async_recursion]
+    pub async fn from_postgres_by_id(
+        oid: u32,
+        client: &Client,
+    ) -> Result<Self, tokio_postgres::Error> {
+        Self::from_postgres("oid = $1", &[&oid], client).await
+    }
+
+    async fn from_postgres(
+        cond: &str,
+        args: &[&(dyn ToSql + Sync)],
+        client: &Client,
+    ) -> Result<Self, tokio_postgres::Error> {
+        let sql =
+            format!("SELECT oid, typname, typtype, typelem FROM pg_catalog.pg_type WHERE {cond}");
+        let row = client.query_one(&sql, args).await?;
+
+        // A Postgres 'char' is represented as an `u8`
+        Ok(match row.get::<_, i8>(2) {
+            // array: 'b' is 98 in ASCII
+            98 => Self::Array(Self::from_postgres_by_id(row.get(3), client).await?.into()),
+            // enum: 'e' is 101 in ASCII
+            101 => Self::Enum(PgEnum::from_postgres(row.get(0), row.get(1), client).await?),
+            ty => todo!(
+                "unknown Postgres type {ty:?} (from name {:?})",
+                row.get::<_, &str>(1)
+            ),
         })
     }
 
@@ -142,6 +168,7 @@ impl fmt::Display for TypeAsRef<'_> {
 
         use Type::*;
         match val {
+            Array(inner) => fmt.write_fmt(format_args!("Vec<{inner}>")),
             Builtin(PgType::INT8) => write!(fmt, "i64"),
             Builtin(PgType::INT4) => write!(fmt, "i32"),
             Builtin(PgType::TEXT) => write!(fmt, "&{}{}{}str", lt_prefix, lt_name, lt_suffix),
